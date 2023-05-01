82c4bbad9e1e82a9d0ae4f383f5ca7acc3d3d170
ayuguchi <ayuguchi@github.com>
2023-05-01 18:56:40 +0900

    WIP: Update the joint info. of commu

--- src/sample_commu.py
+++ src/sample_commu.py
@@ -1,15 +1,15 @@
 # -*- encoding: UTF-8 -*-
 #!/usr/bin/env python3
 import argparse
-import client as cl
+import client_commu as cl
 import time
 
 # MOTION
 # motionとは、poseの系列。poseは、各関節の角度指令値(map)と到達時間(msec)の辞書
 NOD_MOTION = [
-    {"Msec": 250, "ServoMap":{"R_SHOU":105,"HEAD_P":-15,"R_ELBO":0,"L_ELBO":-3,"L_SHOU":-102}},
-    {"Msec": 250, "ServoMap":{"R_SHOU":77,"HEAD_P":20,"R_ELBO":17,"L_ELBO":-17,"L_SHOU":-79}},
-    {"Msec": 250, "ServoMap":{"R_SHOU":92,"HEAD_P":-5,"R_ELBO":5,"L_ELBO":-7,"L_SHOU":-88}},
+    {"Msec": 250, "ServoMap":{"R_SHOU_P":105,"HEAD_P":-15,"R_SHOU_R":0,"L_SHOU_R":-3,"L_SHOU_P":-102}},
+    {"Msec": 250, "ServoMap":{"R_SHOU_P":77,"HEAD_P":20,"R_SHOU_R":17,"L_SHOU_R":-17,"L_SHOU_P":-79}},
+    {"Msec": 250, "ServoMap":{"R_SHOU_P":92,"HEAD_P":-5,"R_SHOU_R":5,"L_SHOU_R":-7,"L_SHOU_P":-88}},
 ]
 
 if __name__ == '__main__':
@@ -38,21 +38,23 @@ if __name__ == '__main__':
     print('Sample.wav', 'time:', t)
     time.sleep(t / 1000 + 3)
 
+    #Motionパートでエラーあり（おそらくrobot typeと相関あり）
+    '''
     # Pose command (Servo)
     text = '2秒間で、45度回転し、左手を90度あげます。'
     t = cl.say_text(HOST, PORT, text)
     print(text, 'time:', t)
-    pose = {'Msec': 2000, 'ServoMap': {'BODY_Y': -45, 'L_SHOU': 0}}
+    pose = {'Msec': 2000, 'ServoMap': {'BODY_Y': -45, 'L_SHOU_P': 0}}
     cl.play_pose(HOST, PORT, pose)
     time.sleep(t / 1000 + 3)
 
     # Pose command (LED)
-    text = '目の色も変えられます。'
-    t = cl.say_text(HOST, PORT, text)
-    print(text, 'time:', t)
-    pose = {'Msec': 500, 'LedMap': {'L_EYE_R': 0, 'L_EYE_G': 0, 'L_EYE_B': 255, 'R_EYE_R': 0, 'R_EYE_G': 0, 'R_EYE_B': 255}}
-    cl.play_pose(HOST, PORT, pose)
-    time.sleep(t / 1000 + 3)
+    #text = '目の色も変えられます。'
+    #t = cl.say_text(HOST, PORT, text)
+    #print(text, 'time:', t)
+    #pose = {'Msec': 500, 'LedMap': {'L_EYE_R': 0, 'L_EYE_G': 0, 'L_EYE_B': 255, 'R_EYE_R': 0, 'R_EYE_G': 0, 'R_EYE_B': 255}}
+    #cl.play_pose(HOST, PORT, pose)
+    #time.sleep(t / 1000 + 3)
 
     # Read command
     text = '今の関節角度をコンソールに表示しています。'
@@ -63,7 +65,7 @@ if __name__ == '__main__':
     time.sleep(t / 1000 + 3)
 
     # Pose command
-    text = '関節角度をリセットして、目の色も戻します。'
+    text = '関節角度をリセットします。'
     t = cl.say_text(HOST, PORT, text)
     print(text, 'time:', t)
     cl.reset_pose(HOST, PORT)
@@ -91,6 +93,7 @@ if __name__ == '__main__':
     print(text, 'time:', t)
     cl.stop_idle_motion(HOST, PORT)
     time.sleep(t / 1000 + 3)
+    '''
 
     # Say with motion
     text = '''\
